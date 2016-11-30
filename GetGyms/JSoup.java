eee639ab2ba6184cc0e21c89c7742f1251572f5f
Emildichmann272 <emil@dichmann.info>
2016-11-30 12:23:38 +0100

    Method now gets string from External site instead of Lectio

--- GetGyms/JSoup.java
+++ GetGyms/JSoup.java
@@ -10,12 +10,10 @@ import org.jsoup.select.Elements;
 
 public class JSoup {
 	public static Map<String, String> Map(String[] args) throws MalformedURLException, IOException {
-			String url = "http://www.lectio.dk/lectio/login_list.aspx?showall=1";
+			String url = "http://www.dichmann.one/lectio/logingym.php";
 	        Document doc = Jsoup.connect(url).get();	        
 	        Map<String, String> values = new HashMap<String, String>();
-	        String text = doc.select("div").first().text();
-	        System.out.println(text);
-	        
+        
 	        Elements links = doc.select("a");
 	        for (Element link : links) {
 	        	values.put(link.attr("href").replace("/lectio/", "").replace("/default.aspx", ""), link.text());
