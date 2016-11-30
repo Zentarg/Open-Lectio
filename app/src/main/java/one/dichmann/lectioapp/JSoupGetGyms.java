20b5c97fb140ce254f9a1733840f8f1119afb63d
Zentarg <apg.chili@gmail.com>
2016-11-30 18:56:43 +0100

    Played a little around with how the app is supposed to get stuff from jsoup, aswell as display it. This commits entire purpose is for me to be able to work on my desktop computer. No actual progress has been made.

---
+++ app/src/main/java/one/dichmann/lectioapp/JSoupGetGyms.java
@@ -0,0 +1,27 @@
+package one.dichmann.lectioapp;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JSoupGetGyms {
+    public static Map<String, String> Map(String[] args) throws MalformedURLException, IOException {
+        String url = "http://www.lectio.dk/lectio/login_list.aspx?showall=1";
+        Document doc = Jsoup.connect(url).get();
+        Map<String, String> values = new HashMap<String, String>();
+        String text = doc.select("div").first().text();
+        System.out.println(text);
+
+        Elements links = doc.select("a");
+        for (Element link : links) {
+            values.put(link.attr("href").replace("/lectio/", "").replace("/default.aspx", ""), link.text());
+        }
+        return values;
+    }
+}
--- app/src/main/java/one/dichmann/lectioapp/LoginActivity.java
+++ app/src/main/java/one/dichmann/lectioapp/LoginActivity.java
@@ -1,10 +1,24 @@
 package one.dichmann.lectioapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class LoginActivity extends AppCompatActivity {
     public final static String SKOLE_ID = "one.dichmann.LectioApp.SKOLEID";
@@ -13,13 +27,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 
-    public void loginOneNext(View view) {
+
+
+    public void loginOneNext(View view, String[] args) throws IOException, MalformedURLException {
         Intent intent = new Intent(this, LoginTwoActivity.class);
         SearchView login_Search = (SearchView) findViewById(R.id.login_Search);
         String id = login_Search.getQuery().toString();
-        intent.putExtra(SKOLE_ID, id);
+        Map<String, String> ids = JSoupGetGyms.Map(args);
+        intent.putExtra(SKOLE_ID, String.valueOf(ids));
         startActivity(intent);
     }
-}
+ }
+
+
+
