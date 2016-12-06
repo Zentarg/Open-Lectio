38b611ea102604259128d7b6ce0ec24356c919ee
LookACastle <thorkik@gmail.com>
2016-12-06 14:04:08 +0100

    Forgot to commit ParseLesson....

--- Projekt/src/downloadLectio/parseLesson.java
+++ Projekt/src/downloadLectio/parseLesson.java
@@ -3,15 +3,28 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.regex.*;
 
-public class parseLesson {
+public class ParseLesson {
+
+	public static String getAdditionalContent(String lessonData) {
+		// Gets the additional content from a lesson.
+		Pattern dateRegex = Pattern.compile("(?<=Øvrigt indhold:)(?s)(.*?)(?=$)");
+		Matcher dateMatcher = dateRegex.matcher(lessonData);
+		
+		if(dateMatcher.find()){
+			return dateMatcher.group(0);
+		}
+		return null;
+	}
 	
 	public static String getDate(String lessonData) {
-		System.out.println(lessonData);
 		// Gets the date from the data it's fed, only matches dates with Lectio or Lectio-like date syntax.
 		Pattern dateRegex = Pattern.compile("([1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9]");
 		Matcher dateMatcher = dateRegex.matcher(lessonData);
 		
-		return dateMatcher.group();
+		if(dateMatcher.find()){
+			return dateMatcher.group(0);
+		}
+		return null;
 	}
 	
 	public static String getHomework(String lessonData) {
@@ -19,57 +32,76 @@ public class parseLesson {
 		Pattern homeworkRegex = Pattern.compile("(?<=Lektier:)(?s)(.*?)(?=Note:|ﾃ�vrigt indhold:|$)");
 		Matcher homeworkMatcher = homeworkRegex.matcher(lessonData);
 		
-		return homeworkMatcher.group();
+		if(homeworkMatcher.find()){
+			return homeworkMatcher.group(0);
+		}
+		return null;
 	}
 	
 	public static String getNote(String lessonData) {
 		// Gets the note section of a lesson from Lectio.
-		Pattern noteRegex = Pattern.compile("(?<=Note:)(?s)(.*?)(?=ﾃ�vrigt indhold:|$)");
+		Pattern noteRegex = Pattern.compile("(?<=Note:)(?s)(.*?)(?=Øvrigt indhold:|$)");
 		Matcher noteMatcher = noteRegex.matcher(lessonData);
 		
-		return noteMatcher.group();
+		if(noteMatcher.find()){
+			return noteMatcher.group(0);
+		}
+		return null;
 	}
 	
 	public static String getRoom(String lessonData) {
 		// Gets the room of a lesson from Lectio.
-		Pattern roomRegex = Pattern.compile("(?<=Lokale: )(?s)(.*?)(?=\n|,|=)");
+		Pattern roomRegex = Pattern.compile("(?<=Lokale: |Lokaler: )(?s)(.*?)(?=§-§|$)");
 		Matcher roomMatcher = roomRegex.matcher(lessonData);
 		
-		return roomMatcher.group();
+		if(roomMatcher.find()){
+			return roomMatcher.group(0);
+		}
+		return null;
 	}
 	
 	public static String getTeacher(String lessonData) {
 		// Gets the teacher of a lesson from Lectio.
-		Pattern teacherRegex = Pattern.compile("(?<=Lﾃｦrer: )(?s)(.*?)(?=\n)");
+		Pattern teacherRegex = Pattern.compile("(?<=Lærer: |Lærere: )(?s)(.*?)(?=§-§)");
 		Matcher teacherMatcher = teacherRegex.matcher(lessonData);
 		
-		return teacherMatcher.group();
+		if(teacherMatcher.find()){
+			return teacherMatcher.group(0);
+		}
+		return null;
 	}
 
 	public static String getTeam(String lessonData) {
 		// Gets the teams participating in a lesson from Lectio.
-		Pattern teamRegex = Pattern.compile("(?<=Hold: )(?s)(.*?)(?=\n)"); // Won't separate the teams if there are more than one, but I won't waste devtime unless it becomes a problem.
+		Pattern teamRegex = Pattern.compile("(?<=Hold: )(?s)(.*?)(?=§-§)"); // Won't separate the teams if there are more than one, but I won't waste devtime unless it becomes a problem.
 		Matcher teamMatcher = teamRegex.matcher(lessonData);
 		
-		return teamMatcher.group();
+		if(teamMatcher.find()){
+			return teamMatcher.group(0);
+		}
+		return null;
 	}
 
 	public static String getTime(String lessonData) {
 		// Gets the time interval where a lesson is happening in Lectio.
-		Pattern timeRegex = Pattern.compile("[0-9][0-9]:[0-9][0-9] til [0-9][0-9]:[0-9][0-9]"); // Won't separate the teams if there are more than one, but I won't waste devtime unless it becomes a problem.
+		Pattern timeRegex = Pattern.compile("(([1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] til [0-9][0-9]:[0-9][0-9])|(([1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] til ([1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9])"); 
 		Matcher timeMatcher = timeRegex.matcher(lessonData);
 		
-		return timeMatcher.group();
+		if(timeMatcher.find()){
+			return timeMatcher.group(0);
+		}
+		return null;
 	}
 
 	public static String getTitle(String lessonData) {
 		// Gets the title of a lesson in Lectio.
-		Pattern titleRegex = Pattern.compile("(?<=\n)(.*?)(?=(\n[1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9])"); 
+		Pattern titleRegex = Pattern.compile("(?<=Ændret!|Aflyst!|^)(.*?)(?=(§-§[1-9]|1[0-9]|2[0-9]|3[0-1])/([1-9]|1[0-2])-[0-9][0-9][0-9][0-9])"); 
 		Matcher titleMatcher = titleRegex.matcher(lessonData);
 		
-		return titleMatcher.group();
-	}
-	public static void main(String[] args) throws MalformedURLException, IOException {
+		if(titleMatcher.find()){
+			return titleMatcher.group(0);
 		}
+		return null;
+	}
 }
 
