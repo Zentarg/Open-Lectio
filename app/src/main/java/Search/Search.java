17c3a0232db4ea2368e9d7aaa6858c7af9cf6797
Emildichmann272 <emil@dichmann.info>
2017-01-02 14:14:10 +0100

    search now regexes with priority of results starting with request and then all other results

--- app/src/main/java/Search/Search.java
+++ app/src/main/java/Search/Search.java
@@ -1,53 +1,18 @@
 package Search;
-
-import android.content.Context;
-import android.content.SyncStatusObserver;
-import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import junit.framework.Assert;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.MalformedURLException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Scanner;
-import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import downloadLectio.AsyncResponse;
-import downloadLectio.GetGyms;
-import one.dichmann.lectioapp.R;
-
-import static android.R.attr.data;
 
 public class Search {
 	private String mResult;
 	private int q = 0;
 	private TextView textView = null;
+	private ImageView imageView = null;
 
-	public void Search(TextView[] textView, String result, String args) {
+	public void Search(ImageView[] imageView, TextView[] textView, String result, String args) {
 		mResult = result;
 
 		String[] key = mResult.split("£");
@@ -55,14 +20,40 @@ public class Search {
 		loop:
 		for (int i = 0; i < key.length; i++) {
 			String[] list = key[i].split("==");
-			Pattern noteRegex = Pattern.compile("^.?" + args + ".*?");
+			Pattern noteRegex = Pattern.compile("^" + args + ".*?");
 			Matcher noteMatcher = noteRegex.matcher(list[0].toLowerCase());
 			if(noteMatcher.find()){
 				textView[q].setText(list[0]);
+				textView[q].setVisibility(View.VISIBLE);
+				imageView[q].setVisibility(View.VISIBLE);
 				q++;
 				if (q==4) {
 					break loop;
 				}
+			}
+		}
+		if (q<4) {
+			loop:
+			for (int i = 0; i < key.length; i++) {
+				String[] list = key[i].split("==");
+				Pattern noteRegex = Pattern.compile(".*?" + args + ".*?");
+				Matcher noteMatcher = noteRegex.matcher(list[0].toLowerCase());
+				if(noteMatcher.find()){
+					textView[q].setText(list[0]);
+					textView[q].setVisibility(View.VISIBLE);
+					imageView[q].setVisibility(View.VISIBLE);
+					q++;
+					if (q==4) {
+						break loop;
+					}
+				}
+			}
+		}
+		if (q<4) {
+			while (q<4){
+				textView[q].setVisibility(View.GONE);
+				imageView[q].setVisibility(View.GONE);
+				q++;
 			}
 		}
 	}
