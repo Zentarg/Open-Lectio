b4572c18598a64dea3139e95b05017eca168170e
Emil Dichmann <emil@dichmann.info>
2017-01-02 21:01:22 +0100

    Changed Search to handle empty strings and return the layout to presearch layout. also added a return to the Search which returns the Ids of the results shown. and a method for seeing which ID to pick according to which gym was picked.

---
+++ app/src/main/java/downloadLectio/GetNames.java
@@ -0,0 +1,37 @@
+package downloadLectio;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.os.AsyncTask;
+
+public class GetNames extends AsyncTask<String, Void, String> {
+    public AsyncResponse delegate = null;
+    private String compact;
+
+    @Override
+    public String doInBackground(String... Strings) {
+        String url = "http://www.enelleranden.dk/lectio/logingym.html";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        Elements links = doc.select("a");
+           for (Element link : links) {
+               compact = compact+link.text() + "==" + link.attr("href").replace("/lectio/", "").replace("/default.aspx", "") + "£";
+           }
+        return compact;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+    }
+}
