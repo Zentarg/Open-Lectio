24989518e83c2d651f0e3d2c7cf2c24457aea3b5
Emildichmann272 <emil@dichmann.info>
2017-01-02 14:41:17 +0100

    created a public variable with the id of the student/gym

--- app/src/main/java/Search/Search.java
+++ app/src/main/java/Search/Search.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 
 public class Search {
 	private String mResult;
+	public String[] IDs;
 	private int q = 0;
 	private TextView textView = null;
 	private ImageView imageView = null;
@@ -23,34 +24,37 @@ public class Search {
 			Pattern noteRegex = Pattern.compile("^" + args + ".*?");
 			Matcher noteMatcher = noteRegex.matcher(list[0].toLowerCase());
 			if(noteMatcher.find()){
+				IDs[q] = list[1];
 				textView[q].setText(list[0]);
 				textView[q].setVisibility(View.VISIBLE);
 				imageView[q].setVisibility(View.VISIBLE);
 				q++;
-				if (q==4) {
+				if (q==textView.length) {
 					break loop;
 				}
 			}
 		}
-		if (q<4) {
+		if (q<textView.length) {
 			loop:
 			for (int i = 0; i < key.length; i++) {
 				String[] list = key[i].split("==");
 				Pattern noteRegex = Pattern.compile(".*?" + args + ".*?");
 				Matcher noteMatcher = noteRegex.matcher(list[0].toLowerCase());
 				if(noteMatcher.find()){
+					IDs[q] = list[1];
 					textView[q].setText(list[0]);
 					textView[q].setVisibility(View.VISIBLE);
 					imageView[q].setVisibility(View.VISIBLE);
 					q++;
-					if (q==4) {
+					if (q==textView.length) {
 						break loop;
 					}
 				}
 			}
 		}
-		if (q<4) {
-			while (q<4){
+		if (q<textView.length) {
+			while (q<textView.length){
+				IDs[q] = "null";
 				textView[q].setVisibility(View.GONE);
 				imageView[q].setVisibility(View.GONE);
 				q++;
