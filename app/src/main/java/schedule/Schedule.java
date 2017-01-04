05746d845fd73d1ad23f5786ccde6ab2991e32eb
Emil Dichmann <emil@dichmann.info>
2017-01-04 19:10:42 +0100

    added comments on Schedule.java and Weekday.java

--- app/src/main/java/schedule/Schedule.java
+++ app/src/main/java/schedule/Schedule.java
@@ -1,46 +1,38 @@
 package schedule;
 
-import android.os.AsyncTask;
+import android.os.AsyncTask; //limits the consumption on the UI thread
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.List;
+import downloadLectio.AsyncResponse; //used to get the product of the Asynctask
+import downloadLectio.parseLesson;   //used to parse the data from the schedule to qualify what kind of data it is
 
-import downloadLectio.AsyncResponse;
-import downloadLectio.GetSchedule;
-import downloadLectio.parseLesson;
-
-public class Schedule extends AsyncTask<String, Void, String> {
-    public AsyncResponse delegate = null;
-    public String[] modules;
-    public String schedule;
-    String[] lesson;
-    String lessons;
+public class Schedule extends AsyncTask<String, Void, String> { //works as a parsing terminal
+    public AsyncResponse delegate = null; //initialises Asyncresponse delegate which should be set to context before doInBackground executes
+    public String[] modules; //a String array of all the modules
+    String lessons; //
 
     @Override
-    public String doInBackground(String... Strings) {
-        modules = Strings[0].split("£");
-        lesson = new String[modules.length];
-        for (int i=0;i<modules.length;i++) {
-            String lessonData = modules[i];
-            String additionalContent = parseLesson.getAdditionalContent(lessonData);
-            String room = parseLesson.getRoom(lessonData);
-            String teacher = parseLesson.getTeacher(lessonData);
-            String time = parseLesson.getTime(lessonData);
-            String team = parseLesson.getTeam(lessonData);
-            String note = parseLesson.getNote(lessonData);
-            String homework = parseLesson.getHomework(lessonData);
-            String title = parseLesson.getTitle(lessonData);
-            if (time != null) {
-                lesson[i] = "££"+time+"---"+team+"---"+room+"---"+teacher+"---"+note+"---"+additionalContent+"---"+homework+"---"+title;
-                lessons = lessons+lesson[i];
+    public String doInBackground(String... Strings) { //gets all the strings send to it from getSchedule
+        modules = Strings[0].split("£"); //creates an array of all the modules
+        for (int i=0;i<modules.length;i++) { //loops through all the modules downloaded
+            String additionalContent = parseLesson.getAdditionalContent(modules[i]); //parses for additionalContent
+            String room = parseLesson.getRoom(modules[i]); //parses for rooms
+            String teacher = parseLesson.getTeacher(modules[i]); //parses for teachers
+            String time = parseLesson.getTime(modules[i]); //parses for time
+            String team = parseLesson.getTeam(modules[i]); //parses for teams
+            String note = parseLesson.getNote(modules[i]); //parses for notes
+            String homework = parseLesson.getHomework(modules[i]); //parses for homework
+            String title = parseLesson.getTitle(modules[i]); //parses for title
+            if (time != null) { //we can´t display the module on the schedule if we don´t know when it is
+                //creates an array of the lessons and sepperates them with "££".
+                lessons = lessons+"££"+time+"---"+team+"---"+room+"---"+teacher+"---"+note+"---"+additionalContent+"---"+homework+"---"+title;
             }
         }
-    return lessons;
+    return lessons; //returns the complete and parsed schedule
     }
 
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(String result) {//recieves the result of the AsyncTask
+        //the result is passed on to the processFinish in the desired activity through the delegates context
         delegate.processFinish(result);
     }
 }
