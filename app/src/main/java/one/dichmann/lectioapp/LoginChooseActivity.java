70bb77caab432e01db32e58942b27e5e45efae63
Andreas Giehm <apg.chili@gmail.com>
2017-01-07 02:13:14 +0100

    Added LoginChooseActivity so we can choose if we want to just find the Student, or login as the Student. Fixed an issue where we had an actionbar on all other than the first activity(Still dont know why that happened)

---
+++ app/src/main/java/one/dichmann/lectioapp/LoginChooseActivity.java
@@ -0,0 +1,13 @@
+package one.dichmann.lectioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_choose);
+    }
+}
